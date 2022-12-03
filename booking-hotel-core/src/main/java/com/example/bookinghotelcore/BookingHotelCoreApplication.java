c901ee97318633039014def9231b68540dd367a2
Alina-Vardanovna <alinavardanovna@gmail.com>
2022-12-04 02:07:46 +0400

    init core module

---
+++ booking-hotel-core/src/main/java/com/example/bookinghotelcore/BookingHotelCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookinghotelcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHotelCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingHotelCoreApplication.class, args);
+    }
+
+}
---
+++ booking-hotel-core/src/test/java/com/example/bookinghotelcore/BookingHotelCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookinghotelcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
