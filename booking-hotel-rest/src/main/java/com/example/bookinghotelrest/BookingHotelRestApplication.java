bf5bebe575c9b80696355156b9a765564c7d018f
Alina-Vardanovna <alinavardanovna@gmail.com>
2022-12-04 02:18:43 +0400

    init rest module

---
+++ booking-hotel-rest/src/main/java/com/example/bookinghotelrest/BookingHotelRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookinghotelrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHotelRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingHotelRestApplication.class, args);
+    }
+
+}
---
+++ booking-hotel-rest/src/test/java/com/example/bookinghotelrest/BookingHotelRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookinghotelrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
