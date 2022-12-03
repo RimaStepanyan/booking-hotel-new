e52db1fc57e2afa82d4c9bb31d3ea3584c530ea9
Alina-Vardanovna <alinavardanovna@gmail.com>
2022-12-04 02:14:32 +0400

    init mvc module

---
+++ booking-hotel-mvc/src/test/java/com/example/bookinghotelmvc/BookingHotelMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookinghotelmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
